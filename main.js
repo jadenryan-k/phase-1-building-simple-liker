c7f7494856d80eed7c57fb9b5e9e88020fce23ac
Jaden Ryan <jaden.kitonga@student.moringaschool.com>
2022-06-17 14:07:28 +0300

    commit message

--- main.js
+++ main.js
@@ -1,12 +1,41 @@
+const { assert } = require("chai");
+
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+function likeHeart(){
+  const error=document.querySelector;('#modal');
+  error.classList.add('hidden')
+  const heart=document.querySelectorAll(".like-gylph");
+  for(let i=0;i<heart.length;i++){
+    heart[i].addEventListener('click',(e)=>{
+      mimicServerCall()
+      .then((res)=>{
+        console.log(res)
+      if(e.target.classList.contains('activated-heart')){
+        e.target.classList.remove('activated-heart');
+        e.target.innerText=EMPTY_HEART;
+      }
+      else{
+        e.target.innerText=FULL_HEART;
+        const heart=e.target.classList;
+        heart.add('activated-heart');
+      }
+      })
+      .catch((err)=>{
+        error.classList.remove('hidden')
+        error.innerText=err.message
+        setTimeout(()=>{
+          error.classList.add('hidden')
+        },3000)
+      })
+    })
+  }
 
-
-
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
